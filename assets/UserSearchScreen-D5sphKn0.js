b75b89e2f5ea536ba66d32806e774dea7837402c
Harshit Padha <harshitpadha@gmail.com>
2026-05-31 01:23:57 +0530

    Updates

---
+++ assets/UserSearchScreen-D5sphKn0.js
@@ -0,0 +1,15 @@
+import{d as r,j as e}from"./index-D7dQi-PS.js";import{U as t}from"./UserSearch-BJUyPvm3.js";import"./index-D7K9_0HI.js";import"./iconBase-CF_tJDXn.js";const o=r.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  align-items: center;
+`,n=r.h2`
+  margin-bottom: 20px;
+  color: #333;
+`,i=r.p`
+  margin-bottom: 30px;
+  color: #666;
+  text-align: center;
+  max-width: 600px;
+`,m=()=>e.jsxs(o,{children:[e.jsx(n,{children:"Find People to Chat With"}),e.jsx(i,{children:"Search for users by username or email to start a secure, end-to-end encrypted conversation."}),e.jsx(t,{})]});export{m as default};
